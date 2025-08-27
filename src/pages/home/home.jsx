6ad1680e2dbff3eaeae0eafdc162060f32442cee
Théo Sommacal <t.somm@yahoo.fr>
2025-08-27 15:09:56 +0200

    Ajustements post-mentorat

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../../components/banner/banner";
+
+const Home = () => {
+    return (
+        <>
+            <Banner />
+            <main className="home">
+
+            </main>
+        </>
+    )
+}
