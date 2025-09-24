1eeeed0a7ce8f63583262c7831e5804967f7a90d
Théo Sommacal <t.somm@yahoo.fr>
2025-09-24 15:29:49 +0200

    Tests suite mentorat

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -6,29 +6,22 @@ const Collapse = ({ title, content }) => {
   const toggleCollapse = () => {
     setIsOpen(!isOpen);
   };
-  // return (
-  //   <div className="collapse" onClick={toggleCollapse}>
-  //     <h1>{title}</h1>
-  //     <p>{content}</p>
-  //   </div>
-  // );
 
   return (
     <div className="collapse">
       <button className="collapse-header" onClick={toggleCollapse}>
-        <h1>{title}</h1>
+        <span className="collapse-title">{title}</span>
         <span className="chevron">{!isOpen ? <img src="src\assets\chevron.png" /> : ""}</span>
       </button>
 
       <div
-        ref={contentRef}
         className="collapse-content"
         style={{
           maxHeight: isOpen ? contentRef.current?.scrollHeight + "px" : "0px",
         }}
       >
-        <div className="collapse-text">
-          <p>{content}</p>
+        <div ref={contentRef} className="collapse-text">
+          {content}
         </div>
       </div>
     </div>
