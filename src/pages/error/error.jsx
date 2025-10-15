6f8591b6ce6835a8bc0bb5105f5e4009463f9b82
Théo Sommacal <t.somm@yahoo.fr>
2025-10-15 11:35:39 +0200

    Page Error fonctionnelle, imports scss pour préparer le styling

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+import "./error.scss";
+
 const Error = () => {
   return (
     <div className="error">
-      <h1>Error Page</h1>
+      <p className="error-404">404</p>
+      <p className="error-text">Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/" className="error-link">
+        Retourner sur la page d'accueil
+      </NavLink>
     </div>
   );
 };
